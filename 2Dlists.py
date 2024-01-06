eb7a8a4a379201251c6b914c57b49d981710e71d
Swan Eain Htun <swaneainhtun1212@gmail.com>
2024-01-06 21:12:58 +0630

    First Commit

---
+++ 2Dlists.py
@@ -0,0 +1,6 @@
+drinks = ["coffee","milkshake","milk tea"]
+dessert = ["pudding","cheese cake","donut"]
+dishes = ["hotpot","fried fish","beef stake"]
+food = [drinks,dessert,dishes]
+print(food)
+print(food[0][1][2])
